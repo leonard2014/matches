fe2148818c41edaa31e82536cff3115ce0cebfec
Leonard Wu <leoxwooaus@gmail.com>
2019-04-16 16:17:48 +1000

    move MainActivity to ui folder

--- app/src/main/java/com/leonard/matches/ui/MainActivity.kt
+++ app/src/main/java/com/leonard/matches/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.leonard.matches
+package com.leonard.matches.ui
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.leonard.matches.R
 
 class MainActivity : AppCompatActivity() {
 
